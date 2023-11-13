dddbe4c09eeda163b016b14a23517c7019aec6bf
hazre <37149950+hazre@users.noreply.github.com>
2023-11-13 20:30:03 +0100

    rewrite

--- ResoniteModUpdater/Program.cs
+++ ResoniteModUpdater/Program.cs
@@ -1,155 +1,145 @@
-﻿using Mono.Cecil;
-using Mono.Cecil.Cil;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Net;
-using System.Net.Http.Headers;
-using System.Security.Cryptography;
-using CommandLine;
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
+using Spectre.Console.Cli;
 
-class Options
+namespace ResoniteModUpdater
 {
-    [Option('t', "path", Required = false, HelpText = "resonite mods path")]
-    public string? path { get; set; }
-
-    [Option('t', "bearerToken", Required = false, HelpText = "The bearer token.")]
-    public string? BearerToken { get; set; }
-}
-
-internal class Program
-{
-    private static async Task Main(string[] args)
+    public static class Program
     {
-        string folderPath = @"C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods";
-        HttpClient client = new HttpClient();
-        Parser.Default.ParseArguments<Options>(args)
-        .WithParsed(options =>
+        public static int Main(string[] args)
         {
-            if (options.BearerToken != null)
+            var app = new CommandApp<DefaultCommand>();
+            app.Configure(config =>
             {
-                string bearerToken = options.BearerToken;
-                AuthenticationHeaderValue authHeaderValue = new AuthenticationHeaderValue("Bearer", bearerToken);
-                client.DefaultRequestHeaders.Authorization = authHeaderValue;
+                config.SetApplicationName("ResoniteModUpdater");
+                config.SetApplicationVersion("2.0.0");
+                config.AddExample($"{Utils.GetDefaultPath()}");
+                config.AddExample($"{Utils.GetDefaultPath()}", "-token xxxxxxxxxxxxxx");
+            });
 
-            }
-            if (options.path != null)
-            {
-                folderPath = options.path;
-            }
-        });
-        client.DefaultRequestHeaders.Add("User-Agent", "Resonite mod updater");
-        string[] dllFiles = Directory.GetFiles(folderPath, "*.dll");
-
-        var urlDictionary = new Dictionary<string, string>();
-
-        foreach (string dllFile in dllFiles)
-        {
-            if (Path.GetFileName(dllFile).StartsWith("_")) continue;
-            var assembly = AssemblyDefinition.ReadAssembly(dllFile);
-            var types = assembly.MainModule.Types;
-
-            PropertyDefinition? linkProperty = null;
-            foreach (var type in types)
-            {
-                if (type.BaseType != null && type.BaseType.Name == "ResoniteMod")
-                {
-                    linkProperty = type.Properties.FirstOrDefault(p => p.Name == "Link");
-                    if (linkProperty != null)
-                    {
-                        break;
-                    }
-                }
-            }
-
-            if (linkProperty != null)
-            {
-                var getterMethod = linkProperty.GetMethod;
-                if (getterMethod.HasBody)
-                {
-                    var instructions = getterMethod.Body.Instructions;
-                    foreach (var instruction in instructions)
-                    {
-                        if (instruction.OpCode == OpCodes.Ldstr)
-                        {
-                            string? url = instruction.Operand as string;
-                            if (Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult)
-                                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps)
-                                && uriResult.Host.EndsWith("github.com"))
-                            {
-                                Console.WriteLine($"Found: {Path.GetFileName(dllFile)}: {url}");
-
-                                urlDictionary[dllFile] = url;
-                                assembly?.Dispose();
-                            }
-                        }
-                    }
-                }
-            }
+            return app.Run(args);
         }
 
-        foreach (var kvp in urlDictionary)
+        public static string AskPath()
         {
-            string dllFile = kvp.Key;
-            string url = kvp.Value;
-
-            string dl = $"https://api.github.com/repos/{url.Split('/')[3]}/{url.Split('/')[4]}/releases/latest";
-            client.DefaultRequestHeaders.Add("Accept", "application/vnd.github+json");
-
-
-        download:
-            HttpResponseMessage response = await client.GetAsync(dl);
-            if (response.IsSuccessStatusCode)
-            {
-                string responseBody = await response.Content.ReadAsStringAsync();
-                dynamic release = JsonConvert.DeserializeObject(responseBody);
-                string tagName = release.tag_name;
-                JArray assets = release.assets;
-                foreach (dynamic asset in assets)
-                {
-                    string fileName = asset.name;
-                    if (fileName.EndsWith(".dll") && urlDictionary.ContainsKey(folderPath + "\\" + fileName))
+            return AnsiConsole.Prompt(
+                new TextPrompt<string>($"Please enter the mods folder path:")
+                    .DefaultValue(Utils.GetDefaultPath())
+                    .DefaultValueStyle("gray")
+                    .PromptStyle("green")
+                    .AllowEmpty()
+                    .ValidationErrorMessage($"[red]That's not a valid directory[/]")
+                    .Validate(path =>
                     {
-                        // Download and replace the DLL file
-                        string downloadUrl = asset.browser_download_url;
-                        Console.WriteLine($"Downloading: {downloadUrl}");
-
-                        // Use the downloadUrl to download the DLL file and replace the existing one
-                        client.DefaultRequestHeaders.Add("Accept", "application/octet-stream");
-                        byte[] downloadedDllBytes = await client.GetByteArrayAsync(downloadUrl);
-                        // Compute the hash of the downloaded DLL
-                        using (var md5 = MD5.Create())
+                        return path switch
                         {
-                            byte[] downloadedHash = md5.ComputeHash(downloadedDllBytes);
-                            string downloadedHashString = BitConverter.ToString(downloadedHash).Replace("-", string.Empty);
+                            _ when string.IsNullOrWhiteSpace(path) => ValidationResult.Success(),
+                            _ when !Directory.Exists(path) => ValidationResult.Error(),
+                            _ => ValidationResult.Success(),
+                        };
+                    }));
+        }
 
-                            // Compute the hash of the existing DLL
-                            byte[] existingDllBytes = File.ReadAllBytes(dllFile);
-                            byte[] existingHash = md5.ComputeHash(existingDllBytes);
-                            string existingHashString = BitConverter.ToString(existingHash).Replace("-", string.Empty);
 
-                            if (downloadedHashString != existingHashString)
-                            {
-                                // Hashes are different, replace the DLL
-                                File.WriteAllBytes(dllFile, downloadedDllBytes);
-                                Console.WriteLine("DLL file replaced.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("DLL file is up to date (hashes match).");
-                            }
-                        }
+        internal sealed class DefaultCommand : Command<DefaultCommand.Settings>
+        {
+            public sealed class Settings : CommandSettings
+            {
+                [Description("Path to resonite mods folder.")]
+                [CommandArgument(0, "[ModsFolder]")]
+                public string? ModsFolder { get; init; }
+
+                [Description("GitHub authentication token to bypass the 60 requests per hour limit. Only necessary if you plan to run the command multiple times within a short period.")]
+                [CommandOption("-t|--token")]
+                public string? Token { get; init; }
+            }
+
+            public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
+            {
+                if (!AnsiConsole.Profile.Capabilities.Interactive)
+                {
+                    AnsiConsole.MarkupLine("[red]Environment does not support interaction.[/]");
+                    return 1;
+                }
+
+                AnsiConsole.Status()
+                    .Start("Starting...", ctx =>
+                    {
+                        Thread.Sleep(1000);
+                        ctx.Status("Checking Arguments...");
+
+                        AnsiConsole.Write(new Padder(new Markup("[yellow]Arguments[/]")).Padding(0, 0));
+
+                        if (!string.IsNullOrEmpty(settings.ModsFolder)) AnsiConsole.Write(new Padder(new Markup("[yellow]+[/] ModsFolder Found, skipping Prompt...")).Padding(1, 0));
+                        if (!string.IsNullOrEmpty(settings.Token)) AnsiConsole.Write(new Padder(new Markup("[yellow]+[/] Github Token Found.")).Padding(1, 0));
+                    });
+
+                AnsiConsole.WriteLine();
+
+                string folderPath = settings.ModsFolder ?? AskPath();
+                AnsiConsole.WriteLine();
+
+                var urls = Utils.GetFiles(folderPath).GetAwaiter().GetResult();
+                AnsiConsole.Status()
+                    .Start("Finding Mods...", ctx =>
+                    {
+                        Thread.Sleep(1000);
+                    });
+
+                try
+                {
+                    if (!urls.Any())
+                    {
+                        throw new Exception("No Mods found to update.");
                     }
                 }
-            }
-            else if (response.StatusCode == HttpStatusCode.Forbidden)
-            {
-                Console.WriteLine("Access to the resource is forbidden. Retrying in 1 minute...");
-                await Task.Delay(TimeSpan.FromMinutes(1));
-                goto download;
+                catch (Exception ex)
+                {
+                    AnsiConsole.MarkupLine($"[red]{ex.Message} Press any key to Exit.[/]");
+                    Console.ReadKey();
+                    return 1; // Return an error code
+                }
+
+
+                AnsiConsole.Write(new Padder(new Markup($"[orange1]Mods ({urls.Count})[/]")).Padding(0, 0));
+
+                AnsiConsole.Status()
+                    .Start("Updating Mods...", ctx =>
+                    {
+                        Thread.Sleep(1000);
+                    });
+
+                var table = new Table();
+                table.Border(TableBorder.None);
+                table.LeftAligned();
+                table.Collapse();
+                table.HideHeaders();
+                table.AddColumns("", "", "");
+                table.Columns[0].Width(1);
+                AnsiConsole.Live(new Padder(table).Padding(1, 0))
+                    .Start(ctx =>
+                    {
+                        foreach (var url in urls)
+                        {
+                            string dllFile = url.Key;
+                            string urlValue = url.Value;
+                            var result = Utils.Download(dllFile, urlValue, settings.Token).GetAwaiter().GetResult();
+                            var text = result switch
+                            {
+                                0 => new List<string> { "+", "[green]Updated[/]" },
+                                1 => new List<string> { "-", "[dim]Up To Date[/]" },
+                                _ => new List<string> { "/", "[red]Something went Wrong[/]" },
+                            };
+                            table.AddRow($"[orange1]{text[0]}[/]", Path.GetFileName(dllFile), $"{text[1]}");
+                            ctx.Refresh();
+                        }
+                    });
+                AnsiConsole.WriteLine();
+                AnsiConsole.MarkupLine($"[slateblue3]Finished Updating mods. Press any key to Exit.[/]");
+                Console.ReadKey();
+                return 0;
             }
         }
-        Console.WriteLine("\r\n");
-        Console.WriteLine("Finished updating mods. Press any key to Exit.");
-        Console.ReadKey();
     }
 }
---
+++ ResoniteModUpdater/utils.cs
@@ -0,0 +1,151 @@
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Spectre.Console;
+
+namespace ResoniteModUpdater
+{
+  public static class Utils
+  {
+    public static string GetDefaultPath()
+    {
+      string defaultPath = "";
+
+      if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+      {
+        defaultPath = Environment.ExpandEnvironmentVariables(@"%PROGRAMFILES(X86)%\Steam\steamapps\common\Resonite\rml_mods");
+      }
+      else if (Environment.OSVersion.Platform == PlatformID.Unix)
+      {
+        defaultPath = Environment.ExpandEnvironmentVariables(@"~/.steam/steam/steamapps/common/Resonite/rml_mods");
+      }
+
+      return defaultPath;
+    }
+
+    public static Task<Dictionary<string, string>> GetFiles(string folderPath)
+    {
+      string[] dllFiles = Directory.GetFiles(folderPath, "*.dll");
+
+      var urlDictionary = new Dictionary<string, string>();
+
+      foreach (string dllFile in dllFiles)
+      {
+        if (Path.GetFileName(dllFile).StartsWith("_")) continue;
+        AssemblyDefinition assembly;
+        try
+        {
+          assembly = AssemblyDefinition.ReadAssembly(dllFile);
+        }
+        catch (Exception ex)
+        {
+          AnsiConsole.MarkupLine($"{Path.GetFileName(dllFile)}: [red]{ex.Message}[/]");
+          continue;
+        }
+        var types = assembly.MainModule.Types;
+
+        PropertyDefinition? linkProperty = null;
+        foreach (var type in types)
+        {
+          if (type.BaseType != null && type.BaseType.Name == "ResoniteMod")
+          {
+            linkProperty = type.Properties.FirstOrDefault(p => p.Name == "Link");
+            if (linkProperty != null)
+            {
+              break;
+            }
+          }
+        }
+
+        if (linkProperty != null)
+        {
+          var getterMethod = linkProperty.GetMethod;
+          if (getterMethod.HasBody)
+          {
+            var instructions = getterMethod.Body.Instructions;
+            foreach (var instruction in instructions)
+            {
+              if (instruction.OpCode == OpCodes.Ldstr)
+              {
+                string? url = instruction.Operand as string;
+                if (Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult)
+                    && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps)
+                    && uriResult.Host.EndsWith("github.com") && uriResult.ToString().TrimEnd('/').Split('/').Length > 4)
+                {
+                  urlDictionary[dllFile] = url;
+                  assembly?.Dispose();
+                }
+              }
+            }
+          }
+        }
+      }
+      return Task.FromResult(urlDictionary);
+    }
+    public static async Task<int> Download(string dllFile, string url, string? token)
+    {
+      HttpClient client = new HttpClient();
+      client.DefaultRequestHeaders.Add("User-Agent", "Resonite mod updater");
+      if (token != null) client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+      download:
+      string dl = $"https://api.github.com/repos/{url.Split('/')[3]}/{url.Split('/')[4]}/releases/latest";
+      client.DefaultRequestHeaders.Add("Accept", "application/vnd.github+json");
+      HttpResponseMessage response = await client.GetAsync(dl);
+      if (response.IsSuccessStatusCode)
+      {
+        string responseBody = await response.Content.ReadAsStringAsync();
+        dynamic release = JsonConvert.DeserializeObject(responseBody)!;
+        string tagName = release.tag_name;
+        JArray assets = release.assets;
+        foreach (dynamic asset in assets)
+        {
+          string fileName = asset.name;
+          if (fileName == Path.GetFileName(dllFile))
+          {
+            // Download and replace the DLL file
+            string downloadUrl = asset.browser_download_url;
+
+            // Use the downloadUrl to download the DLL file and replace the existing one
+            client.DefaultRequestHeaders.Add("Accept", "application/octet-stream");
+            byte[] downloadedDllBytes = await client.GetByteArrayAsync(downloadUrl);
+            // Compute the hash of the downloaded DLL
+            using (var md5 = MD5.Create())
+            {
+              byte[] downloadedHash = md5.ComputeHash(downloadedDllBytes);
+              string downloadedHashString = BitConverter.ToString(downloadedHash).Replace("-", string.Empty);
+
+              // Compute the hash of the existing DLL
+              byte[] existingDllBytes = File.ReadAllBytes(dllFile);
+              byte[] existingHash = md5.ComputeHash(existingDllBytes);
+              string existingHashString = BitConverter.ToString(existingHash).Replace("-", string.Empty);
+
+              if (downloadedHashString != existingHashString)
+              {
+                // Hashes are different, replace the DLL
+                File.WriteAllBytes(dllFile, downloadedDllBytes);
+                return 0;
+              }
+              else
+              {
+                return 1;
+              }
+            }
+          }
+        }
+      }
+      else if (response.StatusCode == HttpStatusCode.Forbidden)
+      {
+        AnsiConsole.MarkupLine("Access to the resource is forbidden. Retrying in 1 minute...");
+        await Task.Delay(TimeSpan.FromMinutes(1));
+        goto download;
+        // throw new Exception("Access to the resource is forbidden.");
+      }
+      return -1;
+    }
+
+  }
+}
